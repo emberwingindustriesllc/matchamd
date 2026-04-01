ec17af49fbffe7de3aad1cf313909da1e04bb136
EmberWing LLC <295496544+emberwingindustriesllc@users.noreply.github.com>
2026-04-01 17:11:09 -0400

    MatchaMD iOS Finalization: Rebranding, IAP Migration, and Safe Areas

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.emberwingindustriesllc.matchamd',
+  appName: 'MatchaMD',
+  webDir: 'dist'
+};
+
+export default config;
