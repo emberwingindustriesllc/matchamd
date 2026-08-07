1d68949c050c983bbdcacefaedea9cc735536b06
EmberWing LLC <emberwingindustriesllc@users.noreply.github.com>
2026-08-07 10:32:55 -0400

    fix(pwa): add service-worker.js to public directory

---
+++ public/service-worker.js
@@ -0,0 +1,8 @@
+// MatchaMD Service Worker
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
